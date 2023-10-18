945ae459490422bb2e95c4f084e34278f4668e0b
Jürmo Mihhailov <mihhailovjurmo@gmail.com>
2023-10-18 12:43:04 +0300

    Read data from file

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 const path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    fs.readFile('tasks.txt', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        const tasks = data.split('\n');
+        res.render('index', { tasks: tasks });
+    });
 });
 
 app.listen(3000, () => {
