eb97f100e02686f6910506ee9d0b8d2f72b062a0
Jürmo Mihhailov <mihhailovjurmo@gmail.com>
2023-10-18 13:00:05 +0300

    Write data to file

--- index.js
+++ index.js
@@ -6,15 +6,42 @@ const path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', (req, res) => {
-    fs.readFile('tasks.txt', 'utf8', (err, data) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        const tasks = data.split('\n');
-        res.render('index', { tasks: tasks });
+const readFile = (filename) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            const tasks = data.split('\n');
+            resolve(tasks);
+        });
     });
+};
+
+app.get('/', (req, res) => {
+    readFile('tasks.txt')
+        .then((tasks) => {
+            console.log(tasks)
+            res.render('index', { tasks: tasks });
+        });
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/', (req, res) => {
+    readFile('tasks.txt')
+        .then(tasks => {
+            tasks.push(req.body.task);
+            const data = tasks.join('\n');
+            fs.writeFile('tasks.txt', data, (err) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                res.redirect('/');
+            });
+    })
 });
 
 app.listen(3000, () => {
